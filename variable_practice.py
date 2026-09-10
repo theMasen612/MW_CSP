49a6ce30ed39f7d66fcaa7ce2b34bd6e9d836553
theMasen612 <masen.waldron@ucas-edu.net>
2026-09-10 13:49:28 -0600

    vairiables practice

---
+++ variable_practice.py
@@ -0,0 +1,13 @@
+# MW, Vairiable practice 
+
+name = "masen"
+number_one =  "9" 
+number_two =  "900" 
+lunch = "taqutos"
+color = "purple" 
+school = "ucas"
+year = "2026"
+eye_color = "brown" 
+someones_age = "13"
+fav_subject = "biolagy" 
+print(name, number_one, number_two, lunch, color, school, year, eye_color, someones_age, fav_subject)
