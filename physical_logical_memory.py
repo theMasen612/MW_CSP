40ccde1ff164278449baea075da795a1e29c0a07
theMasen612 <masen.waldron@ucas-edu.net>
2026-09-11 13:35:43 -0600

    physical logical memory lesson

---
+++ physical_logical_memory.py
@@ -0,0 +1,5 @@
+pi = 3.14159
+everything = 42
+print(f"the variables pi is {pi} and it is saved at {id(pi)}")
+print(f"the variables everything is {everything} and it is saved at {id(everything)}")
+
