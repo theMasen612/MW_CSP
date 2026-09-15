bc6d9c18ddcb1b0fdf9f6784150bcd19cd7347ab
theMasen612 <masen.waldron@ucas-edu.net>
2026-09-15 13:29:47 -0600

    vairiable

---
+++ fixing_imputs.py
@@ -0,0 +1,9 @@
+# M,S fixing imputs 
+
+# when you want a number 
+while True:
+    try:
+        color = input("tell me a color that is only one word: ").lower().strip()
+
+    print(f"I painted the wall with {color}")
+
