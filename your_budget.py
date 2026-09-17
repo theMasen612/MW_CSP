7cc7a993ad7dd96757cd863b0b99d1ea48702bab
theMasen612 <masen.waldron@ucas-edu.net>
2026-09-17 13:50:27 -0600

    your budget

---
+++ your_budget.py
@@ -0,0 +1,20 @@
+# MW, ypur budget 
+
+income = float(input("what is your monthy income: "))
+
+rent =  float(input("what is your monthy rent: "))
+
+utilities =  float(input("what is your monthy utilities bill: "))
+
+groceries =  float(input("what is your monthy groceries bill: "))
+
+transportation =  float(input("what is your monthy transportation bill: "))
+
+housingpercent = 
+
+utilitiespercent = 
+
+groceriespercent = 
+
+
+
