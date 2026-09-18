23272f74b59e819dd326b1683837c7dc2cd91e4c
theMasen612 <masen.waldron@ucas-edu.net>
2026-09-18 13:51:20 -0600

    string

---
+++ strings.py/strings.py
@@ -0,0 +1,12 @@
+# MW, strings 
+first_name = 'masen'
+last_name = "waldron"
+# concatenation => add two strings together
+name = first_name + " " + last_name
+#escape char lets the program ignore the next charecter in the strings 
+print(f'{name} told the class "you can\'t drive my car."') 
+
+user = input("-please tell me your name:\n")
+strip().title()
+
+print(f"New user recognized\nWelcome {user}")
