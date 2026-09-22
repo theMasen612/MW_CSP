7580751b9c0c7415fa33dd915c3e682c450da3c7
theMasen612 <masen.waldron@ucas-edu.net>
2026-09-22 13:16:25 -0600

    silly sentances

--- strings.py/silly_sentances2.py
+++ strings.py/silly_sentances2.py
@@ -1,38 +1,73 @@
 # MW, silly sentances 
-noun1 = input(give me a noun: )
+noun1 = input("Type me a noun: ")
 
-adjective1 = input(give me an adjective: )
+adjective1 = input("Type me an adjective: ")
 
-adjective2 = input(give me an ajective: )
+adjective2 = input("Type me an adjective: ")
 
-part_of_the_body1 = input(give me a part of the human body: )
+part_of_the_body1 = input("Type me a part of the human body: ")
 
-verb1 = input(give me a verb: )
+verb1 = input("Type me a verb: ")
 
-a_place = input(give me a place: )
+a_place = input("Type me a place: ")
 
-celebrity1 = input(give me a celebrity: )
+celebrity1 = input("Type me a celebrity: ")
 
-animal1 = input(give me an animal: )
+animal1 = input("Type me an animal: ")
 
-number1 = input(give me a number: )
+number1 = input("Type me a number: ")
 
-adjictive3 = input(give me an adjective: )
+adjective3 = input("Type me an adjective: ")
 
-lastname1 = input(give me a last name:L )
+lastname1 = input("Type me a last name: ")
 
-noun2 = input(give me another noun: )
+noun2 = input("Type me another noun: ")
 
-type_of_food1 = input(give me a food item: )
+type_of_food1 = input("Type me a food item: ")
 
-person_in_the_room1 = input(give m the name of somone in the room: )
+person_in_the_room1 = input("Type me the name of someone in the room: ")
 
-first_name_male1 = input(give me a males name: )
+first_name_male1 = input("Type me a male's name: ")
 
-verb_ending_in_ing1 = input(give me a verb ending an ing: )
+verb_ending_in_ing1 = input("Type me a verb ending in -ing: ")
 
-exclamation1 = input(give me an exclamation: )
+exclamation1 = input("Type me an exclamation: ")
 
-verb2 = input(give me an verb: )
+verb2 = input("Type me a verb: ")
 
-print("convincing your" +noun1+" to let you stay up past your bedtime to play vidio games can be" +adjective1+" but it's not imposible. here are some" +adjective2+" exuses to use when you need one last game. My" +part_of_the_body1+"hurts. The only way it'll feel better is if I" +verb1+" these cyborgs and save the" +a_place+celebrity1+"also plays" +animal1+" hurt so if you want me to be successful in life, please give me" +number1+" minutes to finish the" +adjictive3+" level. Mrs" +lastname1+", my" +noun2+" teacher, said that vidio games make you smart. she plays" +type_of_food1+" assult, so she knows. There's nothing else to do!" +person_in_the_room1+" "
+
+print(
+    "Convincing your " + noun1 +
+    " to let you stay up past your bedtime to play video games can be " +
+    adjective1 +
+    ", but it's not impossible. Here are some " +
+    adjective2 +
+    " excuses to use when you need one last game. "
+    "My " + part_of_the_body1 +
+    " hurts. The only way it'll feel better is if I " +
+    verb1 +
+    " these cyborgs and save the " +
+    a_place + " " + celebrity1 +
+    " also plays " + animal1 +
+    " hurt, so if you want me to be successful in life, please give me " +
+    number1 +
+    " minutes to finish the " +
+    adjective3 +
+    " level. Mrs. " +
+    lastname1 +
+    ", my " + noun2 +
+    " teacher, said that video games make you smart. "
+    "She plays " + type_of_food1 +
+    " assault, so she knows. "
+    "There's nothing else to do! " +
+    person_in_the_room1 +
+    " is not here to play with, Grandpa. " +
+    first_name_male1 +
+    " went to bed, and it's " +
+    verb_ending_in_ing1 +
+    " outside. " +
+    exclamation1 +
+    "! If they let me play " +
+    verb2 +
+    ", I'll clean my room and think about it."
+)
