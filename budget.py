ce951068d8cd71eb569d7f1d85d507bdbdbc2f49
theMasen612 <masen.waldron@ucas-edu.net>
2026-09-23 19:37:08 +0000

    budget asinment

--- budget.py
+++ budget.py
@@ -1,1 +1,38 @@
-# MW, budget 
+# MW, silly sentances 
+noun1 = input(give me a noun: )
+
+adjective1 = input(give me an adjective: )
+
+adjective2 = input(give me an ajective: )
+
+part_of_the_body1 = input(give me a part of the human body: )
+
+verb1 = input(give me a verb: )
+
+a_place = input(give me a place: )
+
+celebrity1 = input(give me a celebrity: )
+
+animal1 = input(give me an animal: )
+
+number1 = input(give me a number: )
+
+adjictive3 = input(give me an adjective: )
+
+lastname1 = input(give me a last name:L )
+
+noun2 = input(give me another noun: )
+
+type_of_food1 = input(give me a food item: )
+
+person_in_the_room1 = input(give m the name of somone in the room: )
+
+first_name_male1 = input(give me a males name: )
+
+verb_ending_in_ing1 = input(give me a verb ending an ing: )
+
+exclamation1 = input(give me an exclamation: )
+
+verb2 = input(give me an verb: )
+
+print("convincing your" +noun1+" to let you stay up past your bedtime to play vidio games can be" +adjective1+" but it's not imposible. here are some" +adjective2+" exuses to use when you need one last game. My" +part_of_the_body1+"hurts. The only way it'll feel better is if I" +verb1+" these cyborgs and save the" +a_place+celebrity1+"also plays" +animal1+" hurt so if you want me to be successful in life, please give me" +number1+" minutes to finish the" +adjictive3+" level. Mrs" +lastname1+", my" +noun2+" teacher, said that vidio games make you smart. she plays" +type_of_food1+" assult, so she knows. There's nothing else to do!" +person_in_the_room1+" "
